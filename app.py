4bcd514b6bfca1ec610a07c33d794eeb7c67f7f0
Zaid <zaid124ahmed@gmail.com>
2025-06-29 18:43:46 +0530

    refactor: decouple OCR processing from frame rendering loop

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 cap = cv2.VideoCapture(0)
 ocr_data = None
 
-
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    # We need to add the drawing logic here too
-    
+    if ocr_data:
+        n_boxes = len(ocr_data['text'])
+        for i in range(n_boxes):
+            if int(ocr_data['conf'][i]) > 60:
+                x, y, w, h = ocr_data['left'][i], ocr_data['top'][i], ocr_data['width'][i], ocr_data['height'][i]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
     cv2.imshow("Sheet Slayer", frame)
 
     key = cv2.waitKey(1) & 0xFF
@@ -21,8 +25,7 @@ while True:
         break
     
     if key == ord(' '):
-        # The processing logic stays here
-        pass
+        ocr_data = pytesseract.image_to_data(frame, output_type=pytesseract.Output.DICT)
 
 cap.release()
 cv2.destroyAllWindows()
