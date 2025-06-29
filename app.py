b1c79a6b5395d3a215fca3c8c49265c94132f17c
Zaid <zaid124ahmed@gmail.com>
2025-06-29 18:43:34 +0530

    feat: introduce state variable to persist OCR data

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 cap = cv2.VideoCapture(0)
+ocr_data = None
+
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
+    # We need to add the drawing logic here too
+    
     cv2.imshow("Sheet Slayer", frame)
 
     key = cv2.waitKey(1) & 0xFF
@@ -17,18 +21,8 @@ while True:
         break
     
     if key == ord(' '):
-        text = pytesseract.image_to_string(frame)
-        data = pytesseract.image_to_data(frame, output_type=pytesseract.Output.DICT)
-        n_boxes = len(data['text'])
-        for i in range(n_boxes):
-            if int(data['conf'][i]) > 60:
-                x, y, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        if text.strip():
-            print("--- OCR Result ---")
-            print(text)
-            print("--------------------")
+        # The processing logic stays here
+        pass
 
 cap.release()
 cv2.destroyAllWindows()
