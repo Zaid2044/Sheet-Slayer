de3bf802d0521bcf7f3eaa1f11d25bbae3ba093b
Zaid <zaid124ahmed@gmail.com>
2025-06-29 19:27:12 +0530

    feat: add file browser to process static images

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import cv2
 import pytesseract
 import csv
+from tkinter import Tk, filedialog
 
 def process_image_and_get_data(frame):
     data = pytesseract.image_to_data(frame, output_type=pytesseract.Output.DICT)
@@ -52,6 +53,23 @@ while True:
             cv2.putText(frame, "SAVED TO slayed.csv", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             cv2.imshow("Sheet Slayer", frame)
             cv2.waitKey(1000)
+
+    if key == ord('b'):
+        Tk().withdraw()
+        filepath = filedialog.askopenfilename()
+        if filepath:
+            img = cv2.imread(filepath)
+            img_data = process_image_and_get_data(img)
+            
+            n_boxes = len(img_data['text'])
+            for i in range(n_boxes):
+                if int(img_data['conf'][i]) > 60:
+                    x, y, w, h = img_data['left'][i], img_data['top'][i], img_data['width'][i], img_data['height'][i]
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            
+            cv2.imshow("Processed Image", img)
+            cv2.waitKey(0)
+            cv2.destroyWindow("Processed Image")
 
 cap.release()
 cv2.destroyAllWindows()
